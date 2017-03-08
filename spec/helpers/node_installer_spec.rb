63541da32f39e2bee77a21f21bae66ec1bbb3807
Terence Lee <hone02@gmail.com>
2017-03-07 19:54:45 -0600

    fix node installer test

--- spec/helpers/node_installer_spec.rb
+++ spec/helpers/node_installer_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe LanguagePack::NodeInstaller do
   describe "#install" do
     it "should extract a node binary" do
-      installer = LanguagePack::NodeInstaller.new("cedar-14")
+      installer = LanguagePack::NodeInstaller.new
       Dir.mktmpdir do |dir|
         Dir.chdir(dir) do
           installer.install
